8fd5d65984380c3f3d4c9b9f5171a75ece8e27a4
Oliver Lai <oliverlai55@gmail.com>
2018-03-26 12:50:23 -0400

    Add selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -9,12 +9,14 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length, i++) {
     let indexOfMin = i;
 
+    // Start comparing to find the indexOfMin
     for (let j = i+1; j < arr.length; j++) {
       if (arr[j] < arr[indexOfMin]) {
         indexOfMin = j;
       }
     }
 
+    // swapping lesser with indexOfMin
     if (indexOfMin !== i) {
       let lesser = arr[indexOfMin];
       arr[indexOfMin] = arr[i];
