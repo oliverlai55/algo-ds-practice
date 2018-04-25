979f45a37ba7334a8249301bbe641c01e0f2a8e6
Oliver Lai <oliverlai55@gmail.com>
2018-04-25 15:02:08 -0400

    Add notes

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -42,6 +42,16 @@ class Tree {
 			fn(node);
 		}
 	}
+
+	traverseDF(fn) {
+		const arr = [this.root];
+		while (arr.length) {
+			const node = arr.shift();
+
+			arr.unshift(...node.children);
+			fn(node);
+		}
+	}
 }
 
 
